7642f56297e2707be8ed4cb408d192e7ced2a868
Erik-Paulin <erik.paulin@elev.ga.ntig.se>
2024-05-09 18:51:10 +0200

    idk

---
+++ .history/js/main_20240507142104.js
@@ -0,0 +1,49 @@
+let menuBtn = document.querySelector(".nav-menu-btn")
+menuBtn.addEventListener("click",myMenuFunction)
+
+let loginBtn = document.querySelector("#loginBtn")
+loginBtn.addEventListener("click",login)
+
+let registerBtn = document.querySelector("#registerBtn")
+registerBtn.addEventListener("click",register)
+
+let loginBtn = document.querySelector("#loginBtn")
+loginBtn.addEventListener("click",login)
+
+let registerBtn = document.querySelector("#registerBtn")
+registerBtn.addEventListener("click",register)
+
+var a = document.getElementById("loginBtn");
+var b = document.getElementById("registerBtn");
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+
+// Functions är till stor del lånade med specifiering gjort av mig
+
+function myMenuFunction() {
+  var i = document.getElementById("navMenu");
+
+  if (i.className === "nav-menu") {
+    i.className += " responsive";
+  } else {
+    i.className = "nav-menu";
+  }
+}
+
+function login() {
+  x.style.left = "4px";
+  y.style.right = "-520px";
+  a.className += " white-btn";
+  b.className = "btn";
+  x.style.opacity = 1;
+  y.style.opacity = 0;
+}
+
+function register() {
+  x.style.left = "-510px";
+  y.style.right = "5px";
+  a.className = "btn";
+  b.className += " white-btn";
+  x.style.opacity = 0;
+  y.style.opacity = 1;
+}
---
+++ .history/js/main_20240507145208.js
@@ -0,0 +1,28 @@
+let menuBtn = document.querySelector(".nav-menu-btn")
+menuBtn.addEventListener("click",myMenuFunction)
+
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+
+// Functions är till stor del lånade med specifiering gjort av mig
+
+function myMenuFunction() {
+  var i = document.getElementById("navMenu");
+
+  if (i.className === "nav-menu") {
+    i.className += " responsive";
+  } else {
+    i.className = "nav-menu";
+  }
+}
+
+function login() {
+  x.style.left = "5px";
+  y.style.right = "-520px";
+
+}
+
+function register() {
+  x.style.left = "-520px";
+  y.style.right = "5px";
+}
